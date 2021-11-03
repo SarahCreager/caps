8b463004876fd5b02a4eddb86884390f4473ae6c
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-11-02 18:32:34 -0700

    refactored code, added test, updated README

--- Driver/driver.js
+++ Driver/driver.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const parcel = require('./parcel.js');
+const parcel = require('../Hub/parcel.js');
+const logEvent = require('../Hub/eventLogger');
 
 // confirming pickup and emitting in-transit event
 parcel.on('pickup', (payload) => {
-  console.log('EVENT: pickup', payload);
-  console.log(`DRIVER: picked up ${payload.storeInfo.orderID}`);
+  console.log(`DRIVER: picked up ${payload.orderID}`);
 
   parcel.emit('in-transit', payload);
 });
 
 // confirming in-transit and emitting delivered event
 parcel.on('in-transit', (payload) => {
-  console.log('EVENT: in-transit', payload);
-  console.log(`DRIVER: delivered up ${payload.storeInfo.orderID}`);
-
+  logEvent('in-transit');
+  console.log(`DRIVER: delivered up ${payload.orderID}`);
+  
   parcel.emit('delivered', payload);
 });
+
